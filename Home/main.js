ed6de1e916e9e3da863689807101d935f2fccd4c
Abdokechaf <165335731+Abdokechaf@users.noreply.github.com>
2024-06-17 15:41:36 +0100

    Update main.js

--- Home/main.js
+++ Home/main.js
@@ -1,7 +1,7 @@
 // function showingMenu(Menu){
     
 // }
-let menu = document.querySelector("header nav ul li .fa-bars");
+let menu = document.querySelector("header nav ul li .fa-bars" );
 let end = document.querySelector(".secondheader .parts .part1 .bx-x");
 menu.addEventListener("click", function() {
     let parts = document.querySelector(".secondheader");
@@ -11,3 +11,44 @@ end.addEventListener("click", function() {
     let parts = document.querySelector(".secondheader");
     parts.style.right = "100%";
 });
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+  //   direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  }
+  });
